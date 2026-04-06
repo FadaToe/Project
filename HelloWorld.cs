e2e3391141b77d15fcbada7617c5af2aac4b5d9c
FadaToe <fguhov76@gmail.com>
2026-04-06 18:23:17 +0700

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+public class HelloWorld : MonoBehaviour
+{
+    
+    void Start()
+    {
+        print("Hello World");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
